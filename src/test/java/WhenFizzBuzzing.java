8c1ff030dfbdff7a28d5617adf3997f2bda1ae74
Virjanand <virjanand@virjanand.com>
2019-09-15 05:04:17 +0200

    Local variable of desired class

--- src/test/java/WhenFizzBuzzing.java
+++ src/test/java/WhenFizzBuzzing.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ResourceBundle;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WhenFizzBuzzing {
     @Test
     void returnStringForNumber() {
+        FizzBuzz fizzbuzz;
         assertThat(fizzbuzz.getString())
     }
 }
