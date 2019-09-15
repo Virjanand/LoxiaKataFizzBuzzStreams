3d1f0ec37f27a5a01e7e36f7959ae8d148de75f9
Virjanand <virjanand@virjanand.com>
2019-09-15 05:36:28 +0200

    refactor: introduce Map to be able to loop to remove duplication of lines 16 and 17

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FizzBuzz {
     private final int number;
+    private Map<Integer, String> conversionRules = new HashMap<Integer, String>();
 
     public FizzBuzz(int number) {
         this.number = number;
+        conversionRules.put(3, "Fizz");
+        conversionRules.put(5, "Buzz");
     }
 
     public String getString() {
